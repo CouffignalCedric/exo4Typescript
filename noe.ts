792cb2ecdaa201b26dfebe6a71ac475f49b84289
CouffignalCedric <[lecouf@hotmail.fr](mailto:lecouf@hotmail.fr)>
2023-01-05 18:12:59 +0100

    add function

--- noe.ts
+++ noe.ts
@@ -1,37 +1,98 @@
-// Chat européen (noir)
-// Chat chartreux
-// Chien Terre-Neuve (noir)
-// Chien Moon Moon
-// Mésange
-// Merle (noir)
-// Thon
-// Requin
-// Asticot
 interface AllAnimals{
+    species : string;
     name : string;
-    color: string;
+    color?: string;
 }
 class Animals{
-   name :string;
-    color : string;
-    pattes : number;
+    species : string;
+    name :string;
+    color?: string;
 
-     constructor(name: string, color: string, pattes:number) {
-        this.name = name;
-        this.color = color;
-        this.pattes = pattes;
+constructor(species:string, name: string, color?: string) {
+    this.species = species;
+    this.name = name;
+    this.color = color;
+    
+    
 
 }}
+ class Cats extends Animals{
+
+    pattes: number;
+
+ constructor(species:string,name: string, color?: string, pattes: 4){
+    super(species,name, color);
+   
+    this.pattes = pattes;
+ }
+ }
+ 
+ class Dog extends Animals{
+    
+     pattes: number;
+
+ constructor(species:string,name: string, color?: string, pattes: 4){
+    super(species,name, color);
+   
+    this.pattes = pattes;
+ }
+}
+ class Bird extends Animals{
+
+ pattes: number;
+
+ constructor(species:string,name: string, color?: string, pattes: 2){
+    super(species,name, color);
+   
+    this.pattes = pattes;
+
+ }}
+
+
+ class Fish extends Animals{
+     
+
+ constructor(species:string,name: string, color?: string){
+    super(species,name, color);
+   
+    
+
+ }}
+
+
+ class Vers extends Animals{
+   
+
+ constructor(species:string,name: string, color?: string){
+    super(species,name, color);
+   
+    
+ }}
 // On peut photographier tous les animaux
-
-
+function takePhoto(animal:Animals){
+return ("On peut photographier tous les animaux")
+}
 
 // Tous les chats peuvent miauler
+function miauler(animal:Animals){
+    return ("Miaou")};
+
 
 // Tous les chiens peuvent aboyer
+function aboyer(animal:Animals){
+    return ("Wouf")};
 
 // Tous les oiseaux peuvent voler
+function voler(animal:Animals){
+    return ("Tous les oiseaux peuvent voler")};
 
 // Tous les poissons peuvent nager
+function nager(animal:Animals){
+    return ("Tous les poissons peuvent nager")};
 
 // On peut caresser tous les animaux à 4 pattes
+function caresser(animal:Animals){
+    return ("On peut caresser tous les animaux à 4 pattes")};
+// On peut nourrir tous les animaux de couleur noire
+function nourrir(animal:Animals){
+    return ("On peut nourrir tous les animaux de couleur noire")};
