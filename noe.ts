0989048ecd1c8c47e921215a32ee21d2ae6a6ce0
CouffignalCedric <[lecouf@hotmail.fr](mailto:lecouf@hotmail.fr)>
2023-01-05 10:32:29 +0100

    first commit

---
+++ noe.ts
@@ -0,0 +1,37 @@
+// Chat européen (noir)
+// Chat chartreux
+// Chien Terre-Neuve (noir)
+// Chien Moon Moon
+// Mésange
+// Merle (noir)
+// Thon
+// Requin
+// Asticot
+interface AllAnimals{
+    name : string;
+    color: string;
+}
+class Animals{
+   name :string;
+    color : string;
+    pattes : number;
+
+     constructor(name: string, color: string, pattes:number) {
+        this.name = name;
+        this.color = color;
+        this.pattes = pattes;
+
+}}
+// On peut photographier tous les animaux
+
+
+
+// Tous les chats peuvent miauler
+
+// Tous les chiens peuvent aboyer
+
+// Tous les oiseaux peuvent voler
+
+// Tous les poissons peuvent nager
+
+// On peut caresser tous les animaux à 4 pattes
